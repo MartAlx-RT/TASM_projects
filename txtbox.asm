bdf9310c477e8639b2c8b53cf3f9dcc23361cee1
MartAlx-RT <mav07@list.ru>
2026-02-17 01:49:12 +0300

    WORKS!

--- txtbox.asm
+++ txtbox.asm
@@ -15,85 +15,74 @@ RBTM			equ 0bch		; right-bottom ---\\---\\--
 
 SLP_TIME		equ 10d			; pause time sleeping
 
-BOX_WIDTH		equ 20d
 V_STARTPOS		equ 5d
 ;---------------------
 
 .data
-	clr_attr	db	3h
-	str_buf		db	254d, 260 dup(0)
+	clr_attr	db	3h					; used in print functions as color attribute
+	str_buf		db	254d, 260 dup(0)	; for input text
+	box_width	db	20d					; width of box (1...70)
 .code
 org 100h
 
 start proc
+	mov bx, CMDLNSEG
+	cmp byte ptr [bx], 0
+je @@start						; if program runned without arguments
+
+	sub word ptr [bx+2], '0' + '0'*100h
+	mov al, byte ptr [bx+2]
+	mov cl, 10d					; convert ascii to 2-digitals number
+	mul cl
+	add al, byte ptr [bx+3]
+
+	mov box_width, al
+
+@@start:
 	call cls					; clear screen
 
 	mov ah, 0ah
 	mov dx, offset str_buf		; input text
 	int 21h
+	mov bx, offset str_buf+2
+	xor ah, ah
+	mov al, byte ptr [bx-1]
+	add bx, ax
+	mov byte ptr [bx], 0
 
-	mov si, offset str_buf + 2
+	mov si, offset str_buf+2
 	mov cl, byte ptr [si-1]		; print aligning text
 	xor ch, ch
-	call aligned_print
+	call print_aligned
 
-	;int 20h
-	mov cl, BOX_WIDTH+2
-	mov ch, V_STARTPOS - 1		; set es:[di] to left-top corner
+	mov cl, box_width
+	;add cl, 2
+	mov ch, V_STARTPOS-1		; set es:[di] to left-top corner
 	call set_center
+	sub di, 2
 
 	mov bx, bp
 	sub bx, V_STARTPOS
 	mov bh, bl					; print box around the text
-	mov bl, BOX_WIDTH
+	mov bl, box_width
+	;add bl, 2
 	call print_box
 
 	int 20h
+ret
 start endp
 
-;-----COPY CMD ARGUMENTS-------
-;------------------------------
-; es:[di] - offset for copy to 
-; used clr_attr
-;------------------------------
-; DESTR: ax, bx, cx and input parameters
-cmd_cpy proc
-	mov ah, clr_attr
-
-	mov bx, CMDLNSEG 
-	mov cl, [bx]					; cx = strlen, set bx to beginning of the line
-	xor ch, ch
-	add bx, 2
-
-	test cx, cx
-	jz @@exit_cmd_cpy
-
-	dec cl
-	@@cpy:
-		mov al, [bx]
-
-		mov byte ptr es:[di], al	; cp symbol from cmd to screen
-		mov byte ptr es:[di+1], ah	; set color attribute
-		add di, 2
-
-		inc bx
-
-		call pause					; pause
-	loop @@cpy
-
-	@@exit_cmd_cpy:
-ret
-cmd_cpy endp
-;------------------------------
-
-
-; SET ES:[DI] to (bl, bh) ON THE SCREEN
-;------------------------------
+;------------------------------------------------------
+;-----------------SET_XY FUNCTION----------------------
+; SETS ES:[DI] to (bl, bh) ON THE SCREEN
+;-------------------EXPECTED---------------------------
 ; bl - 'x' (0..80)
 ; bh - 'y' (0..25)
+;-------------------RETURNS----------------------------
 ; sets es:[di] to (x, y)
-;------------------------------
-; DESTR: ax, dx and input parameters
+;-------------------DESTROYS---------------------------
+; ax, dx and input parameters
+;------------------------------------------------------
 set_xy proc
 	mov ax, VIDEOSEG
 	mov es, ax				; set es
@@ -109,13 +98,17 @@ set_xy proc
 ret
 set_xy endp
 
-; SET ES:[DI] to print centering line
-;------------------------------
+;------------------------------------------------------
+;-----------------SET_CENTER FUNCTION------------------
+; SETS ES:[DI] to (bl, bh) TO PRINT CENTERING
+;-------------------EXPECTED---------------------------
 ; cl - string length
 ; ch - vertical position
+;-------------------RETURNS----------------------------
 ; sets es:[di] to print centering line
-;------------------------------
-; DESTR: ax, dx and input parameters
+;-------------------DESTROYS---------------------------
+; ax, dx and input parameters
+;------------------------------------------------------
 set_center proc
 	mov ax, VIDEOSEG
 	mov es, ax				; set es
@@ -133,92 +126,81 @@ set_center proc
 ret
 set_center endp
 
-
-; PRINT BOX WITH LEFT-TOP AT ES:[DI]
-;------------------------------
+;------------------------------------------------------
+;-----------------PRINT_BOX  FUNCTION------------------
+; SETS ES:[DI] to (bl, bh) TO PRINT CENTERING
+;-------------------EXPECTED---------------------------
 ; es:[di] - left-top position
 ; bl - width
 ; bh - height
 ; used clr_attr
-;------------------------------
-; DESTR: cx, ax and input parameters
+;-------------------RETURNS----------------------------
+; prints box with left-top at es:[di]
+;-------------------DESTROYS---------------------------
+; cx, ax and input parameters
+;------------------------------------------------------
 print_box proc
 	mov ah, clr_attr
 
 	test bl, bl
-		jz @@exit_print_box			; if you wanna print empty box, go fuck yourself
+		jz @@terminate_print_box			; if you wanna print empty box, go fuck yourself
 
-	;dec bl
 	xor ch, ch						; correcting width && set counter (cx) to zero
+	;dec bl
 
-	;mov byte ptr es:[di], LTOP		; draw left top corner
-	;mov byte ptr es:[di+1], ah
-	;add di, 2
-	cld
-	mov al, LTOP
+	cld								; forward 'stosw' mode
+
+	mov al, LTOP					; draw left-top corner
 	stosw
 
 	mov cl, bl
-	mov al, HLINE
-	;@@top:							; draw top line
-	;	mov byte ptr es:[di], HLINE
-	;	mov byte ptr es:[di+1], ah
-	;	add di, 2
-	;loop @@top
+	mov al, HLINE					; draw upper horizontal line
 	rep stosw
 
-	;mov byte ptr es:[di], RTOP
-	;mov byte ptr es:[di+1], ah		; draw right top corner
-	mov al, RTOP
+	mov al, RTOP					; draw right-top corner
 	stosw
 	add di, LINE_SIZE-2				; -2 because 'stosw' adds 2 to di
 
 	mov cl, bh
 	mov al, VLINE
 	@@right:						; draw right line
-		;mov byte ptr es:[di], VLINE
-		;mov byte ptr es:[di+1], ah
 		stosw
 		add di, LINE_SIZE-2
 	loop @@right
 
-	std
-	;mov byte ptr es:[di], RBTM		; draw right bottom corner
-	;mov byte ptr es:[di+1], ah
-	;sub di, 2
-	mov al, RBTM
+	std								; reverse 'stosw' mode
+
+	mov al, RBTM					; draw right-bottom corner
 	stosw
 
 	mov cl, bl
-	mov al, HLINE
+	mov al, HLINE					; draw lower horizontal line
 	rep stosw
-	;@@bottom:						; draw bottom line
-	;	mov byte ptr es:[di], HLINE
-	;	mov byte ptr es:[di+1], ah
-	;	sub di, 2
-	;loop @@bottom
-
-	mov al, LBTM
-	;mov byte ptr es:[di], LBTM
-	;mov byte ptr es:[di+1], ah		; draw left bottom corner
+	
+	mov al, LBTM					; draw left-bottom corner
 	stosw
 	sub di, LINE_SIZE-2
 
 	mov cl, bh
 	mov al, VLINE
 	@@left:							; draw left line
-		;mov byte ptr es:[di], VLINE
-		;mov byte ptr es:[di+1], ah
 		stosw
 		sub di, LINE_SIZE-2
 	loop @@left
 
-	@@exit_print_box:
+	@@terminate_print_box:
 ret
 print_box endp
 
-;------PAUSE FOR SLP_TIME------
-;---------nothing destroys-----
+;------------------------------------------------------
+;-----------------PAUSE      FUNCTION------------------
+; wait for SLP_TIME microseconds
+;-------------------EXPECTED---------------------------
+; used SLP_TIME
+;-------------------RETURNS----------------------------
+;-------------------DESTROYS---------------------------
+; nothing
+;------------------------------------------------------
 pause proc
 	push ax cx dx
 
@@ -231,8 +213,15 @@ pause proc
 ret
 pause endp
 
-;-------CLEAR SHELL------------
-;------nothing destroys--------
+;------------------------------------------------------
+;-----------------CLEAR SHELL FUNCTION-----------------
+; clears the screen and set default video-mode (number 3)
+;-------------------EXPECTED---------------------------
+;-------------------RETURNS----------------------------
+; cleared screen =)
+;-------------------DESTROYS---------------------------
+; nothing
+;------------------------------------------------------
 cls proc
 	push ax
 
@@ -244,101 +233,80 @@ cls proc
 ret
 cls endp
 
-;--PRINT CENTER-ALIGNED TEXT---
+;------------------------------------------------------
+;-----------------PRINT_ALIGNED FUNCTION---------------
+; prints center-aligned text
+;-------------------EXPECTED---------------------------
 ; si - source text
 ; cx - length of text
 ; used clr_attr
-; Destr: ax, bx, dx, di, bp, es and input parameters
-aligned_print proc
-	;mov bx, si					; use bx for addressing
-	;add bx, cx					; set bx to end of str
-
-	;mov ax, 0ffffh				; 0ffffh - is the 'end' value, used as terminating value
-	;push ax						; push 'end' value to end of stack
-	;push bx
-
-	;@@parse:					; parce (find and push) spaces
-	;	cmp byte ptr [bx], ' '
-	;		jne @@parse_continue
-	;	push bx
-
-	;	@@parse_continue:
-	;	dec bx
-	;	cmp bx, si				; untill line beginning reached
-	;		jg @@parse
-	;;end parse
-
-	;----------------------------------------------------------------------------------
-	mov bp, V_STARTPOS			; begin with V_STARTPOS line, bx - vertical position
+;-------------------RETURNS----------------------------
+; prints center-aligned text
+; beginning with V_STARTPOS line
+;-------------------DESTROYS---------------------------
+; ax, bx, dx, di, bp, es and input parameters
+;------------------------------------------------------
+print_aligned proc
+	mov bp, V_STARTPOS			; begin with V_STARTPOS line, bp - vertical position
 
 	mov bx, si	;				\-\-\-\-\-\-\-\-\-\-\
 	mov dx, si	;				 \       \       \
 				;				 si      dx      bx
 	;           				start  old new   finding new
-	@@print_line:
-		mov si, dx
-	;	@@pop_spc:				; how many spaces is it possible to skip
-	;		mov cx, dx			; how many spaces is it possible to skip
-	;		pop dx				; dx = next space position
 
-	;		mov ax, dx
-	;		sub ax, si
-	;		cmp ax, BOX_WIDTH	; less than BOX_WIDTH?
-	;			jb @@pop_spc	; if less, it may possible to print another word
-	;	; end pop_spc
+	@@print_line:				; external loop that prints line by line
+		mov si, dx				; start with 'old new' position
 
-		;push dx cx
-		@@max_seq:
+		@@max_seq:				; loop that find max sequence that fits in the line
 			mov dx, bx
+
+			cmp byte ptr [bx], 0
+			je @@terminate_print
 
 			@@find_space:
 				inc bx
 
 				cmp byte ptr [bx], 0
-			jz @@exit_print
-				cmp byte ptr [bx-1], ' '
+			je @@end_reached			; check for 'null' or space
+				cmp byte ptr [bx], ' '
 			jne @@find_space
+
+			jmp @@normal_continue	; if end didn't reach, continue normally
+			@@end_reached:
+				;mov bx, -1
+			@@normal_continue:
 
 			mov ax, bx
 			sub ax, si
-			cmp ax, BOX_WIDTH
-		jb @@max_seq
+			cmp al, box_width	; is it fit in the box?
+		jbe @@max_seq			; yes: continue searching
 
-		jmp @@exit_print_last_line
-		@@exit_print:
-		mov dx, bx
-		@@exit_print_last_line:
+		@@terminate_print:		; if end reached, print last string
+		;mov dx, bx
 
 		mov ax, dx
 		sub ax, si				; now, ax = len
-		;test cx, cx
-		;	jz @@exit_print		; len = 0 => exit
-		;inc cx					; correcting length
+		;dec ax
 
-		;push cx
-		;mov ax, bx
-		;mov ch, al
-		;call set_center			; set es:[di] to print aligned line
-		;pop cx
-
-		push dx bx ax
+		push dx bx ax			; save positions
 
 		mov cl, al
-		mov ax, bp
+		mov ax, bp				; set es:[di] to print centering
 		mov ch, al
 		call set_center
 		
-		pop cx
+		pop cx					; restore length
 		call strncpy			; copy current line to vram
 		inc bp					; vertical position ++
 
-		pop bx dx
+		pop bx dx				; restore pointers
 
-		cmp byte ptr [bx], 0
-	jnz @@print_line
+		;cmp byte ptr [dx], 0	; if end of the text didn't reach, continue
+		cmp bx, dx
+	jne @@print_line
 
 ret
-aligned_print endp
+print_aligned endp
 
 
 ;--COPY N BYTES FROM SI TO VIDEO--
@@ -352,14 +320,10 @@ strncpy proc
 	
 	test cx, cx
 		jz @@strncpy_exit			; length = 0 => exit
-	dec cx							; correct length
+	;dec cx							; correct length
 	@@cpy_loop:
 		mov al, byte ptr [si]
-		;mov byte ptr es:[di], al
-		;mov byte ptr es:[di+1], ah
-		stosw
-
-		;add di, 2
+		stosw						; copy to es:[di]
 		inc si
 	loop @@cpy_loop
 
